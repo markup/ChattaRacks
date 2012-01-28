40a4a90c9c9ecfd69f7d7e616d5256cdaf10d03a
Alfonso <alfonso@agarzola.com>
2012-01-28 11:03:08 -0500

    Refactor JS.

--- js/actions.js
+++ js/actions.js
@@ -1,66 +1,83 @@
+// Bunch of global variables:
 var map;
-var directionsDisplay;
+var directionsDisplay = new google.maps.DirectionsRenderer();
 var directionsService = new google.maps.DirectionsService();
+var infowindow = new google.maps.InfoWindow({ content: "Default" });
 var youMarker;
-var infowindow;
+var youMarkerIcon = "img/youMarker.png";
+var rackMarkerIcon = "img/rackMarker.png";
+var placeMarkers = [];
+
+$(document).ready(function() {
+
+// Make mobile Safari scroll to top of document (hide address bar):
+  window.scrollTo(0, 1);
+
+// Initialize map if the browser supports geolocation:
+  if (navigator.geolocation) {
+    initMap();
+  } else {
+    alert('Your browser doesn’t support geolocation.');
+  };
+
+// If broswer is capable of standalone (iOS) and app is not in standalone mode, show call to action:
+  if (("standalone" in window.navigator) && !window.navigator.standalone) {
+    $("#addToHome").show();
+  }
+
+// Do something on reorient (between landscape & portrait):
+  window.onorientationchange = reorient;
+// and call reorient on load:
+  window.setTimeout(reorient, 0);
+
+}).on("click", "#addToHome a", function() {
+
+// Hide call to action:
+  $(this).parent().hide();
+  return false;
+
+}).on("click", ".infoWindow a", function() {
+
+// Calculate route:
+  var coords = $(this).attr("data-location").split(",");
+  calcRoute(coords[0],coords[1]);
+  return false;
+
+}).on("click", ".refreshPosition", function() {
+
+// Refresh user’s position on the map; first remove marker:
+  youMarker.setMap(null);
+// then find a new position from the browser:
+  navigator.geolocation.getCurrentPosition(function(position) {
+
+// then generate new position object, assign it to the marker,
+// add the marker to the map, and center the map on the new position:
+    var newPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    placeYouMarker(newPosition);
+    map.setCenter(newPosition);
+  });
+
+// Finally, close the infowindow:
+  infowindow.close();
+  return false;
+});
 
 function initMap() {
-  directionsDisplay = new google.maps.DirectionsRenderer();
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var point = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-
-      // Initialize the Google Maps API v3
-      map = new google.maps.Map(document.getElementById("leMap"), {
-        zoom: 17,
-        center: point,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      });
-
-      directionsDisplay.setMap(map);
-
-      var rackMarkerIcon = "img/rackMarker.png";
-      infowindow = new google.maps.InfoWindow({
-        content: "Default"
-      });
-      placeYouMarker(point);
-      var youMarkerContent = '<div class="infoWindow">' +
-                             '<h1>This is you!</h1>' +
-                             '</div>';
-      google.maps.event.addListener(youMarker, 'click', function() {
-        infowindow.setContent(youMarkerContent);
-        infowindow.open(map,youMarker);
-      });
-      $.getJSON("data/ChattanoogaBicycleParkingCleaned.json",{
-        format: "json"
-      },function(data) {
-          $.each(data, function(k,v) {
-            if (data[k]["Status"] == "Completed") {
-              var lon = data[k]["X_Cor"];
-              var lat = data[k]["Y_Cor"];
-              var point = new google.maps.LatLng(lat, lon);
-              var marker = new google.maps.Marker({
-                position: point,
-                map: map,
-                title: data[k]["Location Name"]
-              });
-              var markerContent = '<div class="infoWindow">' +
-                                  '<h1>' + data[k]["Location Name"] + '</h1>' +
-                                  '<p>' + data[k]["Spaces"] + '<br>' +
-                                  '<a href="#" data-location="' + lat + ',' + lon + '">Route!</a></p>' +
-                                  '</div>';
-              google.maps.event.addListener(marker, 'click', function() {
-                infowindow.setContent(markerContent);
-                infowindow.open(map,marker);
-              });
-            }
-          })
-        }
-      );
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var point = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    map = new google.maps.Map(document.getElementById("leMap"), {
+      zoom: 17,
+      center: point,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
     });
-  } else {
-    alert('W3C Geolocation API is not available');
-  };
+    directionsDisplay.setMap(map);
+    youMarker = placeYouMarker(point);
+    var youMarkerContent = '<div class="infoWindow">' +
+                           '<h1>This is you!</h1>' +
+                           '</div>';
+    setMarkerWindow(youMarker,youMarkerContent);
+    setLocationMarkers("data/ChattanoogaBicycleParkingCleaned.json");
+  });
 }
 function calcRoute(lat,lon) {
   var destination = new google.maps.LatLng(lat,lon);
@@ -85,8 +102,7 @@ function reorient(e) {
   // $("body > div#container").css("-webkit-transform", !portrait ? "rotate(-90deg)" : "").css ;
 }
 function placeYouMarker(point) {
-  var youMarkerIcon = "img/youMarker.png";
-  youMarker = new google.maps.Marker({
+  return new google.maps.Marker({
     position: point,
     map: map,
     title: "This is you",
@@ -94,28 +110,35 @@ function placeYouMarker(point) {
     zIndex: 300
   });
 }
-$(document).ready(function() {
-  window.scrollTo(0, 1);
-  initMap();
-  if (("standalone" in window.navigator) && !window.navigator.standalone) {
-    $("#addToHome").show();
-  }
-  window.onorientationchange = reorient;
-  window.setTimeout(reorient, 0);
-}).on("click", "#addToHome a", function() {
-  $(this).parent().hide();
-  return false;
-}).on("click", ".infoWindow a", function() {
-  var coords = $(this).attr("data-location").split(",");
-  calcRoute(coords[0],coords[1]);
-  return false;
-}).on("click", ".refreshPosition", function() {
-  youMarker.setMap(null);
-  navigator.geolocation.getCurrentPosition(function(position) {
-    var newPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    placeYouMarker(newPosition);
-    map.setCenter(newPosition);
+function setMarkerWindow(marker,content) {
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(content);
+    infowindow.open(map,marker);
   });
-  infowindow.close();
-  return false;
-});
+}
+function setLocationMarkers(JSONurl) {
+  $.getJSON(JSONurl,
+    { format: "json" },
+    function(data) {
+      $.each(data, function(k,v) {
+        if (data[k]["Status"] == "Completed") {
+          var lon = data[k]["X_Cor"];
+          var lat = data[k]["Y_Cor"];
+          var point = new google.maps.LatLng(lat, lon);
+          var marker = new google.maps.Marker({
+            position: point,
+            map: map,
+            title: data[k]["Location Name"]
+          });
+          var markerContent = '<div class="infoWindow">' +
+                              '<h1>' + data[k]["Location Name"] + '</h1>' +
+                              '<p>' + data[k]["Spaces"] + '<br>' +
+                              '<a href="#" data-location="' + lat + ',' + lon + '">Route!</a></p>' +
+                              '</div>';
+          setMarkerWindow(marker,markerContent);
+          placeMarkers.push(marker);
+        }
+      });
+    }
+  );
+}
